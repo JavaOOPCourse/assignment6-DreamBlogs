5d3a378f15d6db31f5c7844b22cbf28651d7759d
DreamBlogs <150840774+DreamBlogs@users.noreply.github.com>
2026-03-17 17:32:18 +0600

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,32 +1,55 @@
+import java.util.Comparator;
+
 public class Student implements Comparable<Student> {
     private String name;
     private double gpa;
     private int age;
 
-    // TODO: Task 1 — Создай конструктор, который принимает имя, GPA и возраст
+    // ODO: Task 1 — Создай конструктор, который принимает имя, GPA и возраст
     public Student(String name, double gpa, int age) {
-        // заглушка
+        this.gpa = gpa;
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() { return name; }
     public double getGpa() { return gpa; }
     public int getAge() { return age; }
 
-    // TODO: Task 1 — Добавь возможность изменить GPA
+    // ODO: Task 1 — Добавь возможность изменить GPA
     public void setGpa(double newGpa) {
         // заглушка
+        this.gpa = newGpa;
     }
 
-    // TODO: Task 1 (Sorting) — Реализуй Comparable по GPA (по возрастанию)
+    // ODO: Task 1 (Sorting) — Реализуй Comparable по GPA (по возрастанию)
     @Override
     public int compareTo(Student other) {
         // заглушка — верни правильное значение сравнения
+        if (this.gpa > other.gpa) {
+            return 1;
+        } else if (this.gpa < other.gpa) {
+            return -1;
+        }
+
         return 0;
     }
 
     @Override
     public String toString() {
-        // TODO: Сделай красивый вывод
-        return "";
+        // ODO: Сделай красивый вывод
+        return ("Name: "+this.name+" GPA: "+this.gpa+" Age: "+this.age);
+    }
+}
+
+class GPAComparator implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        return Double.compare(s1.getGpa(), s2.getGpa());
+    }
+}
+
+class NameComparator implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        return s1.getName().compareTo(s2.getName());
     }
 }
